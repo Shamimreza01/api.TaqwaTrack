5cfa30b6ad78adf496f90da5bbcbaa1b626443ec
shamimreza01 <srshamimreza97903116@gmail.com>
2025-03-21 00:37:20 +0600

    TaqwaTrack v0 Api

---
+++ homePage.js
@@ -0,0 +1,64 @@
+const homePage = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaqwaTrack API</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+        }
+        h1 {
+            color: #2c3e50;
+        }
+        p {
+            font-size: 18px;
+            color: #555;
+        }
+        .container {
+            max-width: 600px;
+            margin: auto;
+            background: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+        .endpoint {
+            background: #2c3e50;
+            color: white;
+            padding: 10px;
+            border-radius: 5px;
+            display: inline-block;
+            margin-top: 10px;
+        }
+        .endpoint a {
+            color: #fff;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to TaqwaTrack API</h1>
+        <p>Your source for Islamic resources and motivational ayahs.</p>
+        <p>Explore our endpoints:</p>
+        <div class="endpoint">
+            <a href="/FortyMotivationalAyah">View 40 Motivational Ayahs</a>
+        </div>
+         <div class="endpoint">
+            <a href="/FortyRabbanaDua">View 40 Rabbana Dua</a>
+        </div>
+         <div class="endpoint">
+            <a href="/QuranArBnEnAudio">View Full Quran</a>
+        </div>
+    </div>
+</body>
+</html>
+`;
+
+export default homePage;
---
+++ index.js
@@ -0,0 +1,83 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import homePage from "./homePage.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@taqwatrack.iur9l.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.APP_NAME}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    if (!client.topology || !client.topology.isConnected()) {
+      await client.connect();
+      console.log("Connected to MongoDB");
+    }
+
+    const DuaDB = client.db(process.env.MONGO_DB_DUA);
+    const QuranDB = client.db(process.env.MONGO_DB_QURAN);
+
+    app.get("/FortyMotivationalAyah", async (req, res) => {
+      try {
+        const FortyMotivationalAyah = await DuaDB.collection(
+          "FortyMotivationalAyah"
+        )
+          .find({}, { projection: { _id: 0 } })
+          .toArray();
+        res.json(FortyMotivationalAyah);
+      } catch (error) {
+        console.error("Error fetching FortyMotivationalAyah:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/FortyRabbanaDua", async (req, res) => {
+      try {
+        const FortyRabbanaDua = await DuaDB.collection("FortyRabbanaDua")
+          .find({}, { projection: { _id: 0 } })
+          .toArray();
+        res.json(FortyRabbanaDua);
+      } catch (error) {
+        console.error("Error fetching FortyRabbanaDua:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/QuranArBnEnAudio", async (req, res) => {
+      try {
+        const QuranArBnEnAudio = await QuranDB.collection("FullQuran")
+          .find({}, { projection: { _id: 0 } })
+          .toArray();
+        res.json(QuranArBnEnAudio);
+      } catch (error) {
+        console.error("Error fetching Quran data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/", (req, res) => {
+      res.send(homePage);
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
